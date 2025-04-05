a811e3153a4be467f420735ecf8b72b7b9ec3d9b
Pranav <pranav.a@browserstack.com>
2025-04-05 13:19:32 +0530

    Making it thread safe

--- src/RideManager.java
+++ src/RideManager.java
@@ -1,18 +1,18 @@
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RideManager {
     private final PassengerManager passengerManager;
     private final DriverManager driverManager;
     private static RideManager rideManager;
-    private PricingStrategy pricingStrategy;
+    private volatile PricingStrategy pricingStrategy;
     private Queue<Ride> rides;
 
     private RideManager(){
         passengerManager = PassengerManager.getPassengerManagerInstance();
         driverManager = DriverManager.getDriverManagerInstance();
         pricingStrategy = new RegularPricingStrategy();
-        rides = new ConcurrentLinkedDeque<>();
+        rides = new ConcurrentLinkedQueue<>();
     }
 
     public static synchronized RideManager getRideManagerInstance(){
@@ -44,27 +44,33 @@ public class RideManager {
         if(ride.getRideStatus() != RideStatus.REQUESTED){
             throw new Exception("Ride is already in progress/cancelled");
         }
-        ride.setDriver(driver);
-        ride.setRideStatus(RideStatus.ACCEPTED);
-        driver.setDriverStatus(DriverStatus.BUSY);
-        notifyPassenger(ride);
+        synchronized (ride){
+            ride.setDriver(driver);
+            ride.setRideStatus(RideStatus.ACCEPTED);
+            driver.setDriverStatus(DriverStatus.BUSY);
+            notifyPassenger(ride);
+        }
     }
 
     public void startRide(Ride ride) throws Exception{
         if(ride.getRideStatus() != RideStatus.ACCEPTED){
             throw new Exception("Ride needs to be accepted first");
         }
-        ride.setRideStatus(RideStatus.IN_PROGRESS);
-        notifyPassenger(ride);
+        synchronized (ride){
+            ride.setRideStatus(RideStatus.IN_PROGRESS);
+            notifyPassenger(ride);
+        }
     }
 
     public void endRide(Ride ride) throws Exception {
         if(ride.getRideStatus() != RideStatus.IN_PROGRESS){
             throw new Exception("Ride needs to be started first");
         }
-        ride.setRideStatus(RideStatus.COMPLETED);
-        notifyPassenger(ride);
-        rides.remove(ride);
+        synchronized (ride){
+            ride.setRideStatus(RideStatus.COMPLETED);
+            notifyPassenger(ride);
+            rides.remove(ride);
+        }
     }
 
     public void setPricingStrategy(PricingStrategy pricingStrategy) {
